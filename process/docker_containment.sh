2372b693b56b72b7c7060b0f4682236f76d56939
qays9 <qayssarayra.h@gmail.com>
2024-10-03 19:22:47 +0300

    update function docker continer

--- process/docker_containment.sh
+++ process/docker_containment.sh
@@ -2,18 +2,22 @@
 
 MALWARE_NAME=$1
 CONTAINER_NAME=$2
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'  
+BOLD='\033[1m'
+NC='\033[0m'  
 
 if [ -z "$MALWARE_NAME" ] || [ -z "$CONTAINER_NAME" ]; then
     echo "Usage: $0 <malware_name> <container_name>"
     exit 1
 fi
 
-mkdir -p "$CONTAINER_NAME"
 docker rm -f "$CONTAINER_NAME" 2>/dev/null
 
-
 echo "Starting Docker container: $CONTAINER_NAME"
-docker run --name "$CONTAINER_NAME" -d -it --rm -v "$(pwd)/$CONTAINER_NAME:/malware" ubuntu:latest /bin/bash
+
+docker run --name "$CONTAINER_NAME" -d -it --rm ubuntu:latest /bin/bash -c "mkdir -p /malware && /bin/bash"
 if [ $? -ne 0 ]; then
     echo "Failed to run Docker container"
     exit 1
@@ -32,3 +36,11 @@ if [ $? -ne 0 ]; then
     echo "Failed to execute malware in container"
     exit 1
 fi
+
+echo -e "Checking container status:\n"
+docker ps -a | grep "$CONTAINER_NAME"
+
+ 
+echo -e "\n${GREEN}${BOLD}Run the following command to enter the container:${NC}"
+echo -e "${BLUE}${BOLD}docker exec -it $CONTAINER_NAME /bin/bash${NC}"  
+echo -e "${GREEN}${BOLD}You will find the malware inside the /malware folder.${NC}"
