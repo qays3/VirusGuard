78a4815f48b57b0012a970d59ad0520d191aeff5
qays3 <167983721+qays3@users.noreply.github.com>
2024-10-08 18:36:37 +0300

    Update setup.sh with execute all the bash script with permission

--- setup.sh
+++ setup.sh
@@ -15,6 +15,13 @@ showProgressBar
 sudo apt install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo apt-get install jq
+showProgressBar
 
-echo "Docker has been installed and started."
+sudo apt-get install -y jq
+showProgressBar
+
+chmod +x ./process/docker_containment.sh
+chmod +x ./process/signature_control.sh
+chmod +x ./process/terminate_process.sh
+
+echo "Setup complete. Docker and dependencies installed successfully."
