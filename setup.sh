b3e72942d9c93cbfe08f682833198d41f974cfea
qays3 <167983721+qays3@users.noreply.github.com>
2024-10-08 18:38:47 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+BOLD='\033[1m'
+NC='\033[0m'  
+
 function showProgressBar {
-    echo -n "Installing dependencies... ["
+    echo -n -e "${BOLD}${BLUE}Installing dependencies... [${NC}"
     for i in {1..50}; do
         sleep 0.1
         echo -n "="
     done
-    echo "] Done."
+    echo -e "] ${BOLD}${GREEN}Done.${NC}"
 }
 
 sudo apt update
@@ -24,4 +30,4 @@ chmod +x ./process/docker_containment.sh
 chmod +x ./process/signature_control.sh
 chmod +x ./process/terminate_process.sh
 
-echo "Setup complete. Docker and dependencies installed successfully."
+echo -e "${BOLD}${GREEN}Setup complete. Docker and dependencies installed successfully.${NC}"
